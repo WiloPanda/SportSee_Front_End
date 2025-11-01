33afb74eb5f976e1416b5f529c922898703d3b85
WiloPanda <wilona.middendorp@gmail.com>
2025-11-01 17:25:18 +0100

    Congratulations ok

---
+++ SportSeeApp/src/Components/Congratulations.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Congratulations = ({ congratulation}) => {
+  return (
+        <p className='congratulations'>{congratulation}</p>
+  )
+}
+
+export default Congratulations
